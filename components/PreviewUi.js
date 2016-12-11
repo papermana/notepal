be7f5e55b6dde464549af5825a7519702be7efb1
Dominik Rowicki <yzuniversal@gmail.com>
2016-12-11 15:11:33 +0100

    Use remark-react instead of dangerouslySetInnerHtml

--- components/PreviewUi.js
+++ components/PreviewUi.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import remark from 'remark';
-import remarkHtml from 'remark-html';
+import remarkReact from 'remark-react';
 import styles from '../styles/previewStyles';
+
+const remarkInstance = remark().use(remarkReact);
 
 class Preview extends React.PureComponent {
   static propTypes = {
     noteText: React.PropTypes.string.isRequired,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      formattedText: '',
-    };
-  }
-
-  componentDidMount = () => {
-    this.getFormattedText();
-  }
-
-  componentWillUpdate = () => {
-    this.getFormattedText();
-  }
-
-  getFormattedText = () => {
-    remark()
-    .use(remarkHtml)
-    .process(this.props.noteText, (err, file) => {
-      this.setState({
-        formattedText: String(file),
-      });
-    });
-  };
-
   render = () => (
-    <div className={styles}
-      dangerouslySetInnerHTML={{__html: this.state.formattedText}} />
+    <div className={styles}>
+      {remarkInstance.process(this.props.noteText).contents}
+    </div>
   );
 }
 
